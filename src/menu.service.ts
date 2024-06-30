c737765710742693188f2291480febb9229de903
Lev-Shapiro <96536068+Lev-Shapiro@users.noreply.github.com>
2024-06-30 20:41:21 +0300

    Bellissimo

--- src/menu.service.ts
+++ src/menu.service.ts
@@ -1,5 +1,5 @@
 export class MenuService {
-  private BUTTON_WIDTH = 150;
+  private BUTTON_WIDTH = 200;
   private BUTTON_HEIGHT = 50;
 
   canvasW: number;
@@ -22,10 +22,10 @@ export class MenuService {
     const ctx = this.ctx;
 
     const modalWidth = 400;
-    const modalHeight = 200;
+    const modalHeight = 400;
     const modalX = (this.canvasW - modalWidth) / 2;
     const modalY = (this.canvasH - modalHeight) / 2;
- 
+
     // Draw modal background
     ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
     ctx.fillRect(modalX, modalY, modalWidth, modalHeight);
@@ -45,19 +45,28 @@ export class MenuService {
       modalY + 50
     );
 
-    const buttonX = (this.canvasW - this.BUTTON_WIDTH) / 2;
-    const buttonY = (this.canvasH - this.BUTTON_HEIGHT) / 2 + 30;
+    ctx.font = "16px Arial";
 
-    ctx.fillStyle = "#FF5722";
-    ctx.fillRect(buttonX, buttonY, this.BUTTON_WIDTH, this.BUTTON_HEIGHT);
+    ctx.fillText(
+      "Press M to open the menu during the game",
+      this.canvasW / 2,
+      modalY + 100
+    )
 
-    ctx.strokeStyle = "#FFFFFF";
-    ctx.lineWidth = 2;
-    ctx.strokeRect(buttonX, buttonY, this.BUTTON_WIDTH, this.BUTTON_HEIGHT);
+    ctx.fillText(
+      "Press R to reload your gun during the game",
+      this.canvasW / 2,
+      modalY + 150
+    )
 
-    ctx.fillStyle = "#FFFFFF";
-    ctx.font = "20px Arial";
-    ctx.fillText("Continue", this.canvasW / 2, buttonY + 32);
+    ctx.fillText(
+      "Press S to open the shop during the game",
+      this.canvasW / 2,
+      modalY + 200
+    )
+
+
+    this.drawButton(1, "Continue")
   }
 
   handleCanvasClick() {
@@ -69,9 +78,8 @@ export class MenuService {
         if (elapsedTime < 1500) {
           await new Promise((r) => setTimeout(r, 1500 - elapsedTime));
         }
-        
-        const buttonX = (this.canvasW - this.BUTTON_WIDTH) / 2;
-        const buttonY = (this.canvasH - this.BUTTON_HEIGHT) / 2 + 30;
+
+        const { buttonX, buttonY } = this.getButtonCoordsByIndex(1);
 
         const clickX = event.clientX;
         const clickY = event.clientY;
@@ -90,5 +98,32 @@ export class MenuService {
       lastSetupTime = Date.now();
       this.canvas.addEventListener("click", handleClick);
     });
+  }
+
+  private getButtonCoordsByIndex(index: number) {
+    const buttonX = (this.canvasW - this.BUTTON_WIDTH) / 2;
+    const buttonY = (this.canvasH - this.BUTTON_HEIGHT) / 2 + 30 * (index + 1) + this.BUTTON_HEIGHT * index;
+
+    return {
+      buttonX,
+      buttonY
+    }
+  }
+
+  private drawButton(index: number, text: string) {
+    const ctx = this.ctx;
+
+    const { buttonX, buttonY } = this.getButtonCoordsByIndex(index);
+
+    ctx.fillStyle = "#FF5722";
+    ctx.fillRect(buttonX, buttonY, this.BUTTON_WIDTH, this.BUTTON_HEIGHT);
+
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(buttonX, buttonY, this.BUTTON_WIDTH, this.BUTTON_HEIGHT);
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "20px Arial";
+    ctx.fillText(text, this.canvasW / 2, buttonY + 32);
   }
 }
